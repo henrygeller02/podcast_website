164dd6dc622316ff58b60e52d955bc11d2c62ec3
henrygeller02 <78386591+henrygeller02@users.noreply.github.com>
2021-05-07 11:07:13 -0400

    changed the name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-on-heroku'`, () => {
+  it(`should have as title 'senior work project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-on-heroku');
+    expect(app.title).toEqual('senior work project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-on-heroku app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('senior work project app is running!');
   });
 });
