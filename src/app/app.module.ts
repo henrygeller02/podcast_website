b91017f4c2283d64c731dbf2b3133a89013d79ea
henrygeller02 <78386591+henrygeller02@users.noreply.github.com>
2021-05-07 17:20:45 -0400

    new c's

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EpisodesComponent } from './episodes/episodes.component';
+import { EpisodeDetailsComponent } from './episode-details/episode-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EpisodesComponent,
+    EpisodeDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/episode-details/episode-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-episode-details',
+  templateUrl: './episode-details.component.html',
+  styleUrls: ['./episode-details.component.css']
+})
+export class EpisodeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
