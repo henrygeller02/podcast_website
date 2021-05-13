af3150d8507b66a1b5650337936150ea650cc764
henrygeller02 <78386591+henrygeller02@users.noreply.github.com>
2021-05-12 21:55:03 -0400

    1st episode upload

--- src/app/episodelist.ts
+++ src/app/episodelist.ts
@@ -1,10 +1,10 @@
 import { Episode } from './episodetemp';
 export const EPISODES: Episode[] = [
-    { id: 1, name: 'PFF', url:'' },
-    { id: 2, name: 'NFL Draft Talk', url:'' },
-    { id: 3, name: 'Ravens Talk', url:'' },
-    { id: 4, name: 'AFC North', url:'' },
-    { id: 5, name: 'O\'s talk', url:'' },
-    { id: 6, name: 'Free Agency talk', url:'' },
-    { id: 7, name: 'NBA playoffs', url:'' },
+    { id: 1, name: 'Episode 1: NFL Draft Talk', url:'https://anchor.fm/henry-geller/embed/episodes/Episode-1-NFL-Draft-Talk-e10oint' },
+    // { id: 2, name: 'NFL Draft Talk', url:'' },
+    // { id: 3, name: 'Ravens Talk', url:'' },
+    // { id: 4, name: 'AFC North', url:'' },
+    // { id: 5, name: 'O\'s talk', url:'' },
+    // { id: 6, name: 'Free Agency talk', url:'' },
+    // { id: 7, name: 'NBA playoffs', url:'' },
   ];
